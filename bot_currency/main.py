c3d29a9ce6b60b2a93487a3956c935a30f8b4efe
Jess <photofridzon@gmail.com>
2023-01-11 12:10:53 +0700

    cur

--- bot_currency/main.py
+++ bot_currency/main.py
@@ -9,22 +9,22 @@ def get_usd():
     result = []
     res = requests.get('https://www.cbr-xml-daily.ru/daily_json.js').json()
     result = res['Valute']["USD"]['Name'], res['Valute']["USD"]['Value']
-    values = ' '.join(str(i) for i in result)
-    return(values)
+    values_usd = ' '.join(str(i) for i in result)
+    return(values_usd)
 
 def get_eur():
     result = []
     res = requests.get('https://www.cbr-xml-daily.ru/daily_json.js').json()
     result = res['Valute']["EUR"]['Name'], res['Valute']["EUR"]['Value']
-    values = ' '.join(str(i) for i in result)
-    return(values)
+    values_eur = ' '.join(str(i) for i in result)
+    return(values_eur)
 
 def get_cny():
     result = []
     res = requests.get('https://www.cbr-xml-daily.ru/daily_json.js').json()
     result = res['Valute']["CNY"]['Name'], res['Valute']["CNY"]['Value']
-    values = ' '.join(str(i) for i in result)
-    return(values)
+    values_cny = ' '.join(str(i) for i in result)
+    return(values_cny)
 
 @bot.message_handler(commands=['start'])
 def start_game(message):
@@ -36,7 +36,7 @@ def start_game(message):
     bot.send_message(message.chat.id, 'Доступен курс на 3 валюты: доллар, евро, китайский юань')
 
 
-@bot.message_handler(content_types=['text'])
+@bot.message_handler(func=get_usd())
 def send_text(message):
     if message.text.lower == 'usd' or 'доллар':
         bot.send_message(message.chat.id, get_usd())
