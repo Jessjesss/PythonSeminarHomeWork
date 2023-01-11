10e70efe29df9ab0d37913c2e00b136ee291858c
Jess <photofridzon@gmail.com>
2023-01-11 22:45:49 +0700

    botready

--- bot_currency/main.py
+++ bot_currency/main.py
@@ -1,19 +1,8 @@
 import telebot
 import requests
-import json
 from cfg import TOKEN
 
 bot = telebot.TeleBot(TOKEN)
-
-# def load_exchange():
-#     res = 'https://www.cbr-xml-daily.ru/daily_json.js'.json()
-#     return json.loads(requests.get(res).text) 
-
-# def get_exchange(ccy_key):  
-#     for exc in load_exchange():  
-#         if ccy_key == exc['ccy']:  
-#             return exc  
-#     return False  
 
 def get_usd():
     result = []
@@ -38,24 +27,24 @@ def get_cny():
 
 @bot.message_handler(commands=['start'])
 def start_game(message):
-    bot.send_message(message.chat.id, f'Привет, напиши название валюты и я пришлю тебе актуальный курс!')
+    bot.send_message(message.chat.id, f'Привет, напиши код валюты и я пришлю тебе актуальный курс!')
 
 
 @bot.message_handler(commands=['help'])
 def start_game(message):
-    bot.send_message(message.chat.id, 'Доступен курс на 3 валюты: доллар, евро, китайский юань')
+    bot.send_message(message.chat.id, 'Доступен курс на 3 валюты: доллар, евро, китайский юань. \nПример ввода: USD - ответ: Доллар США (курс)')
 
 
 @bot.message_handler(content_types=['text'])
 def send_text(message):
-    if message.text.strip() == ('usd' or 'доллар' or 'Доллар'):
+    if message.text.strip().upper() == 'USD':
         bot.send_message(message.chat.id, get_usd())
-    elif message.text.strip() == ('eur' or 'евро' or 'Евро'):
+    elif message.text.strip().upper() == 'EUR':
         bot.send_message(message.chat.id, get_eur())
-    elif message.text.strip() == ('cny' or 'юань' or 'Юань'):
+    elif message.text.strip().upper() == 'CNY':
         bot.send_message(message.chat.id, get_cny())
     else:
-        bot.send_message(message.chat.id, 'Напишите: доллар или usd,\n евро или eur, \n юань или cny!')
+        bot.send_message(message.chat.id, 'Введите: USD, EUR или CNY!')
 
 
 bot.infinity_polling()
