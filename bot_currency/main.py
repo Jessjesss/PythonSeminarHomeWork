801b41fdd458e054baa7825994a7433f7dee8e6b
Jess <photofridzon@gmail.com>
2023-01-11 22:30:33 +0700

    fff

--- bot_currency/main.py
+++ bot_currency/main.py
@@ -1,9 +1,19 @@
 import telebot
 import requests
+import json
 from cfg import TOKEN
 
 bot = telebot.TeleBot(TOKEN)
 
+# def load_exchange():
+#     res = 'https://www.cbr-xml-daily.ru/daily_json.js'.json()
+#     return json.loads(requests.get(res).text) 
+
+# def get_exchange(ccy_key):  
+#     for exc in load_exchange():  
+#         if ccy_key == exc['ccy']:  
+#             return exc  
+#     return False  
 
 def get_usd():
     result = []
@@ -38,25 +48,14 @@ def start_game(message):
 
 @bot.message_handler(content_types=['text'])
 def send_text(message):
-    if message.text.lower == 'usd' or 'доллар':
-        result = []
-        res = requests.get('https://www.cbr-xml-daily.ru/daily_json.js').json()
-        result = res['Valute']["USD"]['Name'], res['Valute']["USD"]['Value']
-        values_usd = ' '.join(str(i) for i in result)
-        bot.send_message(message.chat.id, values_usd)
-    elif message.text.lower == 'eur' or 'евро':
-        result = []
-        res = requests.get('https://www.cbr-xml-daily.ru/daily_json.js').json()
-        result = res['Valute']["EUR"]['Name'], res['Valute']["EUR"]['Value']
-        values_eur = ' '.join(str(i) for i in result)
-        bot.send_message(message.chat.id, values_eur)
-    elif message.text.lower == 'cny' or 'юань' or 'китайский юань':
-        result = []
-        res = requests.get('https://www.cbr-xml-daily.ru/daily_json.js').json()
-        result = res['Valute']["CNY"]['Name'], res['Valute']["CNY"]['Value']
-        values_cny = ' '.join(str(i) for i in result)
-        bot.send_message(message.chat.id, values_cny)
+    if message.text.strip() == 'usd' or 'доллар' or 'Доллар':
+        bot.send_message(message.chat.id, get_usd())
+    elif message.text.strip() == 'eur' or 'евро' or 'Евро':
+        bot.send_message(message.chat.id, get_eur())
+    elif message.text.strip() == 'cny' or 'юань' or 'Юань':
+        bot.send_message(message.chat.id, get_cny())
     else:
-        bot.send_message(message.chat.id, 'Введите доллар, евро или юань!')
+        bot.send_message(message.chat.id, 'Напишите: доллар или usd,\n евро или eur, \n юань или cny!')
+
 
 bot.infinity_polling()
